090789d8be86531cf7edb96427c7e5f8de1fa2a9
LuisMiguelRodriguez <luis.the.coder@gmail.com>
2017-09-20 15:56:23 -0700

    Update db connection for remote JAWSDB

--- config/connection.js
+++ config/connection.js
@@ -1,17 +1,29 @@
 var mysql = require('mysql');
+var connection;
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  // Your username
-  user: "root",
-  // Your password
-  password: "root",
-  database: "burgers_db"
-});
+// var connection = mysql.createConnection({
+//   host: "localhost",
+//   // Your username
+//   user: "root",
+//   // Your password
+//   password: "root",
+//   database: "burgers_db"
+// });
+
+if(process.env.JAWSDB_URL){
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host:"xq7t6tasopo9xxbs.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+    user: "o7apxvruzwnfwvvh",
+    password: "cap8qpl60dj1omo7"
+  });
+}
 
 connection.connect(function(err) {
   if (err) throw err;
   ///Functions to run
+  
 
 });
 
